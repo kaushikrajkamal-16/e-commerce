83cfc4c24758844c90d7d7dd202460ed75adf3e7
unknown <kaushik.rajkamal@gmail.com>
2026-02-07 04:20:50 +0600

    Mobile Navbar created

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,56 +1,166 @@
-import React, { Children } from "react";
+import React, { Children, useEffect, useRef, useState } from "react";
+import { CgChevronDown, CgChevronRight } from "react-icons/cg";
 import { CiSearch, CiShoppingCart } from "react-icons/ci";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { ImCross } from "react-icons/im";
 import { IoChevronDown } from "react-icons/io5";
 import { LuUser } from "react-icons/lu";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { Link } from "react-router";
 
 const Navbar = () => {
+  const [dropdown, setDropDown] = useState("");
+  const [isopen, setIsOpen] = useState(false);
+  const sideref = useRef(null);
+
+  useEffect(() => {
+    document.addEventListener("mousedown", (e) => {
+      if (sideref.current && !sideref.current.contains(e.target)) {
+        setIsOpen(false);
+      }
+    });
+  }, [sideref]);
+
   const categories = [
     {
       title: "Groceries",
-      Children: ["Nokia", "Iphone", "Samsung", "IQoo", "Motrola"],
+      to: "/Groceries",
+      Children: [
+        {
+          title: "Nokia",
+          to: "/Nokia",
+        },
+        {
+          title: "Iphone",
+          to: "/Iphone",
+        },
+        {
+          title: "Samsung",
+          to: "/Samsung",
+        },
+        {
+          title: "IQoo",
+          to: "/IQoo",
+        },
+        {
+          title: "Motrola",
+          to: "/Motrola",
+        },
+      ],
     },
     {
       title: "Premium Fruits",
-      Children: ["Nokia", "Iphone", "Samsung", "IQoo", "Motrola"],
+      to: "/Premium Fruits",
+
+      Children: [
+        {
+          title: "Nokia",
+          to: "/Nokia",
+        },
+        {
+          title: "Iphone",
+          to: "/Iphone",
+        },
+        {
+          title: "Samsung",
+          to: "/Samsung",
+        },
+        {
+          title: "IQoo",
+          to: "/IQoo",
+        },
+        {
+          title: "Motrola",
+          to: "/Motrola",
+        },
+      ],
     },
     {
       title: "Home & Kitchen",
-      Children: ["Nokia", "Iphone", "Samsung", "IQoo", "Motrola"],
+      to: "/Home & Kitchen",
+      Children: [
+        {
+          title: "Nokia",
+          to: "/Nokia",
+        },
+        {
+          title: "Iphone",
+          to: "/Iphone",
+        },
+        {
+          title: "Samsung",
+          to: "/Samsung",
+        },
+        {
+          title: "IQoo",
+          to: "/IQoo",
+        },
+        {
+          title: "Motrola",
+          to: "/Motrola",
+        },
+      ],
     },
     {
       title: "Fashion",
-      Children: ["Nokia", "Iphone", "Samsung", "IQoo", "Motrola"],
+      to: "/Fashion",
+      Children: [
+        {
+          title: "Nokia",
+          to: "/Nokia",
+        },
+        {
+          title: "Iphone",
+          to: "/Iphone",
+        },
+        {
+          title: "Samsung",
+          to: "/Samsung",
+        },
+        {
+          title: "IQoo",
+          to: "/IQoo",
+        },
+        {
+          title: "Motrola",
+          to: "/Motrola",
+        },
+      ],
     },
     {
       title: "Electronics",
-      Children: ["Nokia", "Iphone", "Samsung", "IQoo", "Motrola"],
-    },
-    {
-      title: "Beauty",
-      Children: ["Nokia", "Iphone", "Samsung", "IQoo", "Motrola"],
-    },
-    {
-      title: "Home Improvement",
-      Children: ["Nokia", "Iphone", "Samsung", "IQoo", "Motrola"],
-    },
-    {
-      title: "Sports, Toys & Luggage",
-      Children: ["Nokia", "Iphone", "Samsung", "IQoo", "Motrola"],
+      to: "/Electronics",
+      Children: [
+        {
+          title: "Nokia",
+          to: "/Nokia",
+        },
+        {
+          title: "Iphone",
+          to: "/Iphone",
+        },
+        {
+          title: "Samsung",
+          to: "/Samsung",
+        },
+        {
+          title: "IQoo",
+          to: "/IQoo",
+        },
+        {
+          title: "Motrola",
+          to: "/Motrola",
+        },
+      ],
     },
   ];
 
   return (
-    <>
+    <header>
       <nav>
         {/* Desktop Navbar */}
         <div className='container px-3.5 py-8 hidden 3xl:flex  justify-between items-center'>
           <div className='flex items-center gap-2.5'>
-            <button className='cursor-pointer text-2xl bg-[#F3F9FB] p-2 rounded-lg text-brand 3xl'>
-              <RxHamburgerMenu />
-            </button>
             <Link to='/'>
               <img src='/logo.png' alt='logo' />
             </Link>
@@ -84,7 +194,10 @@ const Navbar = () => {
         <div className='3xl:hidden container px-3.5 '>
           <div className='py-4 flex flex-col'>
             <div className='flex justify-between items-center'>
-              <button className='cursor-pointer text-2xl bg-[#F3F9FB] p-2 rounded-lg text-brand'>
+              <button
+                onClick={() => setIsOpen(true)}
+                className='visible 5xl:invisible cursor-pointer text-2xl bg-[#F3F9FB] p-2 rounded-lg text-brand 3xl'
+              >
                 <RxHamburgerMenu />
               </button>
               <Link to='/'>
@@ -108,29 +221,34 @@ const Navbar = () => {
           </div>
         </div>
       </nav>
-      {/* Product Category Start  */}
-      <section className='hidden 5xl:block  border-y-2 border-[#EDEDED] py-4 px-3.5'>
-        <div className='container flex gap-3.5'>
+      {/* Product Category Desktop Start  */}
+      <section className='hidden 3xl:block  border-y-2 border-[#EDEDED] py-4 px-3.5'>
+        <div className='container flex  gap-3.5'>
           {categories.map((item) => (
             <div key={item.title} className='group relative'>
               <Link
                 className='inline-block px-3.5 rounded-3xl py-2.5  bg-secondary hover:bg-brand hover:text-white transition  '
-                to='/Product'
+                to={item.to}
               >
                 <div className='flex items-center gap-3.5 text-sm'>
                   <div>
                     <p>{item.title}</p>
                   </div>
                   <div>
-                    <IoChevronDown className='inline-block text-brand   group-hover:text-white' />
+                    <button>
+                      <IoChevronDown className='inline-block text-brand   group-hover:text-white' />
+                    </button>
                   </div>
                 </div>
               </Link>
               <ul className='absolute bg-white top-full left-0 invisible opacity-0 group-hover:visible group-hover:opacity-100  w-48 shadow rounded-2xl'>
-                {item.Children.map((grandchild) => (
-                  <li>
-                    <Link className='hover:bg-brand hover:text-white block p-2 rounded-2xl transition '>
-                      {grandchild}
+                {item.Children.map((child) => (
+                  <li key={child.title}>
+                    <Link
+                      to={child.to}
+                      className='hover:bg-brand hover:text-white block p-2 rounded-2xl transition '
+                    >
+                      {child.title}
                     </Link>
                   </li>
                 ))}
@@ -139,9 +257,59 @@ const Navbar = () => {
           ))}
         </div>
       </section>
+      {/* Mobile Sidebar */}
+      <div
+        className={`fixed ${isopen ? "visible  opacity-100" : "invisible  opacity-0"} transition top-0 left-0 w-full h-screen bg-primary/25 backdrop-blur-[1px]`}
+      >
+        <div
+          ref={sideref}
+          className={`${isopen ? "translate-x-0" : "-translate-x-full"} duration-500 transition-all w-4/5 md:w-2/4 h-full bg-white`}
+        >
+          <div className='bg-[#c9e9f6]/30 p-3 flex justify-between items-center'>
+            <h4 className='text-3xl text-primary font-bold'>MainMenu</h4>
+            <button onClick={() => setIsOpen(false)} className='cursor-pointer'>
+              <ImCross className='text-2xl mt-1 text-primary' />
+            </button>
+          </div>
+          <ul className='space-y-2 text-2xl text-primary font-bold p-4 mb-3 border-b-2 border-primary/50'>
+            {categories.map((item) => (
+              <li key={item.title}>
+                <div className='flex justify-between items-center'>
+                  <Link to={item.to} className='bg-white'>
+                    {item.title}
+                  </Link>
+                  <button
+                    onClick={() => setDropDown(item.title)}
+                    className={`${dropdown == item.title ? "hidden" : "block"} `}
+                  >
+                    <CgChevronRight className='mt-2' />
+                  </button>
+                  <button
+                    onClick={() => setDropDown(null)}
+                    className={`${dropdown == item.title ? "block" : "hidden"}`}
+                  >
+                    <CgChevronDown className='mt-2' />
+                  </button>
+                </div>
 
-      {/* Product Category End  */}
-    </>
+                <ul
+                  className={`${dropdown == item.title ? "block" : "hidden"} text-xl space-y-1 text-primary/90 font-semibold px-2`}
+                >
+                  {item.Children.map((child) => (
+                    <li key={child.title}>
+                      <Link to={child.to}>-{child.title}</Link>
+                    </li>
+                  ))}
+                </ul>
+              </li>
+            ))}
+          </ul>
+          <Link className='px-4  text-2xl font-bold text-primary '>
+            Sign Up/Sign In
+          </Link>
+        </div>
+      </div>
+    </header>
   );
 };
 
